b65f602f57fd1225c1e477a851117c0abde90744
juliusiws <julius.hagen@imwebsein.de>
2015-06-19 11:30:10 +0200

    Update local-config.php

--- local-config.php
+++ local-config.php
@@ -6,7 +6,7 @@ In it, you *must* include the four main database defines
 You may include other settings here that you only want enabled on your local development checkouts
 */
 
-define( 'DB_NAME', 'gopagoda' );
-define( 'DB_USER', 'elvina' );
-define( 'DB_PASSWORD', 'mYAoTWfR' );
-define( 'DB_HOST', '192.168.0.4' ); // Probably 'localhost'
+define('DB_NAME', $_SERVER['gopagoda']);
+define('DB_USER', $_SERVER['elvina']);
+define('DB_PASSWORD', $_SERVER['mYAoTWfR']);
+define('DB_HOST', $_SERVER['192.168.0.4']);t'
